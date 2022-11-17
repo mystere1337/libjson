ef1d6e558689893ac64eb470ab30b299456a17af
mystère <admin@mystere.dev>
2022-11-17 18:57:10 +0900

    Split public interface and private functions

--- src/json.c
+++ src/json.c
@@ -8,6 +8,133 @@
 #include <stdio.h>
 #include <sys/mman.h>
 #include <ctype.h>
+#include <stddef.h>
+#include <sys/queue.h>
+
+/**
+ * Enumerates types of JSON values
+ */
+enum json_value_type_e {
+    JSON_VALUE_BOOL,
+    JSON_VALUE_INT,
+    JSON_VALUE_FLOAT,
+    JSON_VALUE_STR,
+    JSON_VALUE_OBJ,
+    JSON_VALUE_ARR,
+    JSON_VALUE_NULL,
+};
+
+/**
+ * Enumerates types of tokens used by the lexer
+ */
+enum json_token_type_e {
+    JSON_TOKEN_BOOL,
+    JSON_TOKEN_NUMBER,
+    JSON_TOKEN_SYNTAX,
+    JSON_TOKEN_NULL,
+    JSON_TOKEN_STRING,
+};
+
+/**
+ * Enumerates types of containers that can be used as root
+ */
+enum json_container_type_e {
+    JSON_CONTAINER_ARRAY,
+    JSON_CONTAINER_OBJ,
+};
+
+typedef struct json_container_s json_container_t;
+typedef struct json_obj_s json_obj_t;
+typedef struct json_setting_s json_setting_t;
+typedef struct json_array_s json_array_t;
+typedef struct json_value_s json_value_t;
+typedef struct json_string_s json_string_t;
+typedef struct json_token_s json_token_t;
+
+/**
+ * Contains a JSON token for parsing
+ */
+struct json_token_s {
+    enum json_token_type_e type;
+    json_string_t* string;
+
+    TAILQ_ENTRY(json_token_s) next;
+};
+
+/**
+ * Contains a JSON container, either Object or Array
+ */
+struct json_container_s {
+    enum json_container_type_e type;
+
+    union {
+        json_array_t* array;
+        json_obj_t* obj;
+    };
+};
+
+/**
+ * Contains a json object ({"key1": true, "key2": false})
+ */
+struct json_obj_s {
+    json_setting_t** settings;
+    size_t settings_count;
+};
+
+/**
+ * Contains a string without null terminator
+ */
+struct json_string_s {
+    char* value;
+    size_t len;
+};
+
+/**
+ * Contains a json string (boolean, string, number, object or array)
+ */
+struct json_value_s {
+    enum json_value_type_e type;
+
+    union {
+        int bool_type;
+        long long long_type;
+        long double double_type;
+        json_string_t* string_type;
+        json_obj_t* obj_type;
+        json_array_t* array_type;
+    };
+};
+
+/**
+ * Contains an array of values ([4, true, false, {}, null, "hello"])
+ */
+struct json_array_s {
+    json_value_t** values;
+    size_t values_count;
+};
+
+/**
+ * Contains a JSON setting ("key": values)
+ */
+struct json_setting_s {
+    json_string_t* name;
+    json_value_t* value;
+};
+
+json_array_t* json_parse_array(json_token_t* first);
+json_obj_t* json_parse_object(json_token_t* first);
+
+void json_free_array(json_array_t* array);
+void json_free_value(json_value_t* value);
+void json_free_setting(json_setting_t* setting);
+void json_free_object(json_obj_t* obj);
+
+void json_dump_string(json_string_t* str, int fd);
+void json_dump_value(json_value_t* val, int format, int fd);
+void json_dump_setting(json_setting_t* set, int format, int fd);
+void json_dump_object(json_obj_t* obj, int format, int fd);
+void json_dump_array(json_array_t* arr, int format, int fd);
+void json_dump_container(json_container_t* con, int format, int fd);
 
 TAILQ_HEAD(json_tailhead_s, json_token_s) json_head;
 
@@ -315,7 +442,7 @@ void json_free_object(json_obj_t* obj) {
  * Frees a JSON container.
  * @param obj object to free
  */
-void json_free_container(json_container_t* con) {
+void json_free(json_container_t* con) {
     if (con->type == JSON_CONTAINER_ARRAY) {
         json_free_array(con->array);
     } else {
@@ -590,7 +717,7 @@ json_container_t* json_parse_container(json_token_t* elem) {
     }
 
     // error: invalid container (container must be either an object or an array)
-    json_free_container(container);
+    json_free(container);
     return NULL;
 }
 
@@ -783,7 +910,7 @@ char* json_get_file_content(int fd) {
 }
 
 /**
- * Creates a new configuration object, creates necessary file if it doesn't exist.
+ * Creates a new configuration object, creates necessary file with default object if the specified file doesn't exist.
  * @param obj pointer to config object holder
  * @param path path to the config file.
  * @return -1 on error, 0 on success
@@ -826,6 +953,6 @@ int json_save(json_container_t* json, const char* path) {
         return 0;
     }
 
-    json_dump_container(json, 1, fd);
+    json_dump_container(json, 0, fd);
     return 1;
 }
